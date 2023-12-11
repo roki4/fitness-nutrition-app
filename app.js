a07dc690bab6a92b7cbf7cff0a40783bf76e5a85
Mark Gavrilov <mark.gavrilov.2018@list.ru>
2023-12-11 05:31:20 +0300

    testing ideas

--- app.js
+++ app.js
@@ -1,13 +1,51 @@
-const express = require('express');
-const mysql2 = require('mysql2');
-const sequelize = require('sequelize');
-const TelegramBot = require('node-telegram-bot-api');
-const helper = require('./helper.js');
-const config = require('./config/index.js');
+const express = require("express");
+const mysql2 = require("mysql2");
+const sequelize = require("sequelize");
+const TelegramBot = require("node-telegram-bot-api");
+const helper = require("./src/helper.js");
+const config = require("./config/index.js");
 
 const bot = new TelegramBot(config.TOKEN, { polling: true });
+let goal_for_day = 1;
 
-bot.onText(/\/start/, (msg) => {
-  const text = `Здравствуйте, ${helper.getUser(msg)}!`;
-  bot.sendMessage(helper.getChatId(msg), text);
+bot.onText(/\/start/, async (msg) => {
+  const text = `Здравствуйте, ${msg.from.first_name}!\n Нажмите ТЫК, чтобы отметитить, что вы поработали сегодня 30 минут.`;
+
+  await bot.sendMessage(helper.getChatId(msg), text, {
+    reply_markup: {
+      keyboard: [["ТЫК"]],
+    },
+  });
+});
+
+bot.onText(/ТЫК/, async (msg) => {
+  if (goal_for_day === 8) {
+    await bot.sendMessage(
+      helper.getChatId(msg),
+      `${helper.convertCountToTime(
+        goal_for_day
+      )}\nВы прозанимались сегодня 4 часа! Можете отдыхать.`,
+      {
+        reply_markup: {
+          keyboard: [["ТЫК"]],
+        },
+      }
+    );
+    await bot.sendMessage(helper.getChatId(msg), "🎉");
+
+    goal_for_day = 0;
+  }
+
+  await bot.sendMessage(
+    helper.getChatId(msg),
+
+    `Ваш счёт: ${goal_for_day} - (${helper.convertCountToTime(goal_for_day)})`,
+    {
+      reply_markup: {
+        keyboard: [["ТЫК"]],
+      },
+    }
+  );
+
+  goal_for_day++;
 });
--- src/helper.js
+++ src/helper.js
@@ -5,4 +5,15 @@ module.exports = {
   getUser(msg) {
     return msg.from.first_name;
   },
+  convertCountToTime(count) {
+    count *= 30;
+    let m;
+    if (count % 60 === 30) {
+      m = '30';
+    } else {
+      m = '0';
+    }
+    let h = String(Math.floor(count / 60));
+    return `${h.padStart(2, '0')}:${m.padStart(2, '0')}`
+  },
 };
