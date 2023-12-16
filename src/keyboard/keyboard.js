cf17e2faecb3d6beb3c83ac345bbf1dad2341197
Mark <93087612+roki4@users.noreply.github.com>
2023-12-16 22:56:23 +0300

    Update keyboard.js

--- src/keyboard/keyboard.js
+++ src/keyboard/keyboard.js
@@ -1,9 +1,6 @@
+const kb = require('./keyboard-button.js');
+
 module.exports = {
-  home: {
-    start_clicker_four_hour: '4-часовой кликер',
-  },
-  clicker_four_hour_menu: {
-    to_home: 'В главное меню',
-    click: 'ТЫК(+30 мин)'
-  }
+  home: [[kb.home.start_clicker_four_hour], []],
+  clicker_four_hour_menu: [[kb.clicker_four_hour_menu.click],[kb.clicker_four_hour_menu.to_home]]
 }
